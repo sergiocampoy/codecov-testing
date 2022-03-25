9dc559abc4a8debfeefe680511ecf64d5733ed29
Sergio Campoy <scampoy@rti.com>
2022-03-25 10:27:15 +0100

    TEST-1: Added new function without adding tests

--- module2.c
+++ module2.c
@@ -15,3 +15,13 @@ void feed(enum ANIMAL animal_found) {
             printf("Unknown animal\n");
     }
 }
+
+void feast(enum ANIMAL hunter, enum ANIMAL prey) {
+    if (hunter > prey) {
+        printf("yummy\n");
+    } else if (hunter) {
+        printf("ded\n");
+    } else {
+        printf("tie\n");
+    }
+}
